c029659b7f7345132242e818abb1896b2857609f
ajanco <apjanco@gmail.com>
2019-12-10 09:42:22 -0500

    altered models to shift from annotator.js to custom UI

--- cadet_app/models.py
+++ cadet_app/models.py
@@ -1,19 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.contrib.postgres.fields import JSONField
 
 
 class Project(models.Model):
     title = models.CharField(max_length=220, blank=True, null=True)
     image = models.ImageField()
-    owners = models.ManyToManyField(User, related_name='owners')
-    editors = models.ManyToManyField(User, related_name='editors')
-    annotators = models.ManyToManyField(User, related_name='annotators')
+    owners = models.ManyToManyField(User, related_name="owners")
+    editors = models.ManyToManyField(User, related_name="editors")
+    annotators = models.ManyToManyField(User, related_name="annotators")
     description = models.TextField(blank=True, null=True)
     language = models.CharField(max_length=220, blank=True, null=True)
     language_object = models.CharField(max_length=220, blank=True, null=True)
     spaCy_model = models.CharField(max_length=220, blank=True, null=True)
-    datasets = models.ManyToManyField("DataSet")
+    datasets = models.ManyToManyField("DataSet", blank=True, default=None)
 
     NO_REVIEW = "NR"
     SINGLE = "SR"
@@ -30,20 +29,38 @@ class Project(models.Model):
         return f"{self.title}"
 
 
-class Text(models.Model):
-    text = models.TextField(blank=True, null=True)
-    datasets = models.ManyToManyField('DataSet')
+class Dataset(models.Model):
+    title = models.CharField(max_length=220, blank=True, null=True)
+    texts = models.ManyToManyField("Text")
+    language = models.CharField(max_length=220, blank=True, null=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    source = models.URLField(max_length=1000, blank=True)
 
     def __str__(self):
         return f"{self.title}"
 
 
-class Dataset(models.Model):
-    title = models.CharField(max_length=220, blank=True, null=True)
-    texts = models.ManyToManyField(Text)
-    language = models.CharField(max_length=220, blank=True, null=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    source = models.URLField(max_length=1000, blank=True)
+class Text(models.Model):
+    text = models.TextField(blank=True, null=True)
+    datasets = models.ManyToManyField("DataSet")
+
+    def __str__(self):
+        return f"{self.title}"
+
+
+class Sentence(models.Model):
+    text = models.TextField(blank=True, null=True)
+    parent = models.ForeignKey(Text, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.title}"
+
+
+class Token(models.Model):
+    parent_text = models.ForeignKey(Text, on_delete=models.CASCADE)
+    parent_sentence = models.ForeignKey(Sentence, on_delete=models.CASCADE)
+    text = models.TextField(blank=True, null=True)
+    annotations = models.ManyToManyField("Annotation", related_name="token_annotations")
 
     def __str__(self):
         return f"{self.title}"
@@ -59,7 +76,7 @@ class LabelGroup(models.Model):
 
 class Label(models.Model):
     label = models.CharField(max_length=220, blank=True, null=True)
-    value = models.CharField(max_length=220, blank=True, null=True)
+    values = models.ManyToManyField("Value")
     shortcut_key = models.CharField(max_length=220, blank=True, null=True)
     color = models.CharField(max_length=220, blank=True, null=True)
 
@@ -70,14 +87,34 @@ class Label(models.Model):
             return f"{self.label}"
 
 
+class Value(models.Model):
+    value = models.CharField(max_length=220, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.value}"
+
+
 class Annotation(models.Model):
-    json = JSONField()
+    labels = models.ManyToManyField(Label)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    text = models.ForeignKey(Text, on_delete=models.CASCADE)
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    dataset = models.ForeignKey("DataSet", on_delete=models.CASCADE, default=None)
+    text = models.ForeignKey(
+        Text, on_delete=models.CASCADE, related_name="annotation_text"
+    )
+    start_char = models.IntegerField(default=None)
+    end_char = models.IntegerField(default=None)
+    sentence = models.ForeignKey(
+        Text, on_delete=models.CASCADE, related_name="annotation_sentence", default=None
+    )
+    token = models.ForeignKey(Text, on_delete=models.CASCADE, default=None)
+    span = models.ManyToManyField(Token)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="annotation_author"
+    )
     approved = models.BooleanField()
+    auto_generated = models.BooleanField(default=None)
 
     def __str__(self):
         return f"{self.id}"
