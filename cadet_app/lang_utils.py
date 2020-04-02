6596e09104e62b789089272e17d653089dcd3391
ajanco <apjanco@gmail.com>
2020-04-02 15:16:30 -0400

    working on language.py modal to modify stop words

--- cadet_app/lang_utils.py
+++ cadet_app/lang_utils.py
@@ -12,15 +12,15 @@ def load_lemmata():
     project = get_object_or_404(Project, id=request.session.get("project_id"))
     language = project.spacy_language.slug.replace('-','_')
     try:
-        path = Path(settings.CUSTOM_LANGUAGES_DIRECTORY + '/lookups-data/') 
+        path = Path(settings.CUSTOM_LANGUAGES_DIRECTORY + '/lookups-data/')
         filename = language + '_lemma_lookup.json.gz'
         path = path / filename
         print(str(path))
         assert path.exists()
-        with gzip.open(str(path), 'rb') as f: 
+        with gzip.open(str(path), 'rb') as f:
             return json.load(f)
     except AssertionError:
-        print('fish eat other fish')# TODO add create lemmata json file 
+        print('fish eat other fish')# TODO add create lemmata json file
 
 def create_model(language, model_path):
     pipeline = ["tagger", "parser", "ner","sentencizer","entity_linker"]
@@ -30,27 +30,28 @@ def create_model(language, model_path):
         component = nlp.create_pipe(name)   # 3. Create the pipeline components
         nlp.add_pipe(component)             # 4. Add the component to the pipeline
     nlp.to_disk(model_path)
+    assert spacy.load(model_path)
 
 def clone_model(language, new_model_path, model):
-    
-    cls = spacy.util.get_lang_class(language)  # Is this necessary? from_disk loads all the meta.  
+
+    cls = spacy.util.get_lang_class(language)  # Is this necessary? from_disk loads all the meta.
     nlp = cls()
-    
+
     # get the current pipelines and data from the core model
-    model = spacy.load(model) 
+    model = spacy.load(model)
     for name in model.pipe_names:
         component = nlp.create_pipe(name)
         nlp.add_pipe(component)
     nlp = nlp.from_disk(str(model.path))
     nlp.to_disk(str(new_model_path))
-    #TODO update meta.json at new_model_path. Change lang from core to cadet lang class  
+    #TODO update meta.json at new_model_path. Change lang from core to cadet lang class
 
     # works to this point ^ not down there v
 
 def add_stuff_below_to_clone_model(): #TODO get rid of this when finished
     # add Cadet's standard pipelines to the model as needed
     full_pipeline = ["tagger", "parser", "ner","sentencizer","entity_linker"]
-    needed = set(full_pipeline).difference(set(model.pipe_names)) 
+    needed = set(full_pipeline).difference(set(model.pipe_names))
     for name in needed:
         component = nlp.create_pipe(name)   # 3. Create the pipeline components
         nlp.add_pipe(component)             # 4. Add the component to the pipeline
@@ -60,7 +61,7 @@ def add_stuff_below_to_clone_model(): #TODO get rid of this when finished
     nlp.to_disk(str(new_model_path))
 
 def create_stop_words(path):
-    path.mkdir(parents=True, exist_ok=False)
+    path.mkdir(parents=True, exist_ok=True)
     path = path / 'stop_words.py'
 
     with path.open('w', encoding="utf-8") as f:
@@ -78,7 +79,7 @@ STOP_WORDS = set(
 
 def create_examples(path):
     examples = path / 'examples.py'
-    with examples.open('w', encoding="utf-8") as f: 
+    with examples.open('w', encoding="utf-8") as f:
         f.write(blank_examples)
 
 def create_spacy_language(language):
@@ -88,9 +89,9 @@ def create_spacy_language(language):
     spacy_path = Path(spacy.__file__.replace('__init__.py',''))
     spacy_lang = spacy_path / 'lang' / language
     spacy_lang.symlink_to(path)
-    
+
     init = path / '__init__.py'
-    with init.open('w', encoding="utf-8") as f: 
+    with init.open('w', encoding="utf-8") as f:
         f.write(
 f"""
 from spacy.lang.{language}.stop_words import STOP_WORDS
@@ -128,14 +129,14 @@ __all__ = ["{language.capitalize()}"]
     model_path = Path(settings.CUSTOM_LANGUAGES_DIRECTORY + '/models/' + language)
     model_path.mkdir(parents=True, exist_ok=False)
     create_model(language, model_path)
-# still needs creation of lookups path 
+# still needs creation of lookups path
 
 
 def clone_spacy_language(language, clone, model=None):
     language = slugify(language).replace('-','_')
     new_path = Path(settings.CUSTOM_LANGUAGES_DIRECTORY + '/lang/' + language)
     new_path.mkdir(parents=True, exist_ok=False)
-    
+
     # Create symlink between spacy/lang and the new custom languages directory
     lang_path = Path(spacy.__file__.replace('__init__.py','')) / 'lang' / language
     lang_path.symlink_to(new_path)
@@ -146,14 +147,14 @@ def clone_spacy_language(language, clone, model=None):
         core_path = Path(settings.CUSTOM_LANGUAGES_DIRECTORY + '/lang/' + clone.language)
         assert core_path.exists()
 
-    # copy files from core to custom 
+    # copy files from core to custom
     core_files = [x for x in core_path.glob('**/*') if x.is_file() and 'pyc' not in str(x)]
     for src in core_files:
-        dest = new_path / src.name  
+        dest = new_path / src.name
         copyfile(src, dest)
 
     # Edit imports and variable names
-    
+
     language_name = spacy.util.get_lang_class(clone.iso).__name__
     init = new_path / '__init__.py'
     init_text = init.read_text()
@@ -176,7 +177,7 @@ def clone_spacy_language(language, clone, model=None):
     spacy_lookups = Path(spacy_lookups_data.__file__.replace('__init__.py','')) / 'data'
     assert spacy_lookups.exists()
 
-    # Use the iso code to identify lookups-data files 
+    # Use the iso code to identify lookups-data files
     new_lookups = Path(settings.CUSTOM_LANGUAGES_DIRECTORY + '/lookups-data/')
     lookups_files = [x for x in spacy_lookups.glob('**/*') if x.is_file() and clone.iso+'_' in str(x)]
     for src in lookups_files:
@@ -184,7 +185,7 @@ def clone_spacy_language(language, clone, model=None):
         dest = new_lookups / new_name
         copyfile(src, dest)
 
-    #Create or clone a spaCy model using the language object 
+    #Create or clone a spaCy model using the language object
     model_path = Path(settings.CUSTOM_LANGUAGES_DIRECTORY + '/models/' + language)
     model_path.mkdir(parents=True, exist_ok=False)
     if model:
@@ -197,4 +198,3 @@ def clone_spacy_language(language, clone, model=None):
         create_model(language, model_path)
 
     # TODO copy lookups tests
-        
